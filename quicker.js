27cdf210284a8d7fbf8a4852c6d1d75d8336f242
LuisArmando-TestCoder <37167842+LuisArmando-TestCoder@users.noreply.github.com>
2019-01-13 00:00:33 -0600

    No more Eric Meyer

--- quicker.js
+++ quicker.js
@@ -94,41 +94,8 @@ function quick() {
     });
   } // in func, this.json -> response
   
-  // Consult Erik's Code
-  // At <<https://blog.dubbelboer.com/2012/12/21/getParameterByName-getCookieByName.html>>
-  /* Get parameter by name function */
-  function getParameterByName(name) {
-      var res = new RegExp(
-          // Parameter names always start after a ? or &.
-          '[\?&]' +
-
-          // Make sure any [ or ] are escaped in the name.
-          name.replace(/\[/g, '\\\[').replace(/\]/g, '\\\]') +
-
-          // Either match a =... or match an empty value.
-          // Values can be terminated by an & a # or the end of the string ($).
-          '(?:=([^&#]*))?(?:[&#]|$)'
-      ).exec(window.location.search);
-
-      return res ?
-        (res[1] ? // res[1] will be undefined for a parameter without value.
-          decodeURIComponent(res[1].replace(/\+/g, ' ')) : ''
-        ) : null;
-  }
-  
-  function getCookieByName(name) {
-    // According to RFC 2109 cookies can either be separated by ';' or ','.
-    var res = new RegExp(
-      // Beginning of the string or just after the previous cookie.
-      // Skip the whitespace.
-      '(?:^|[,;])\\s*' +
-  
-      name +
-  
-      // Value ending in a ';', ',' or the end of the string.
-      '=([^,;]*)(?:[,;]|$)').exec(document.cookie);
-  
-    return res ? res[1] : null;
+  function getUrlVariableValueByName(variableKey = new RegExp('\\??&?(\\w{0,})=(\\w{0,})', 'g').exec(window.location.href)[1]) {
+    return new RegExp(`\\??&?(${variableKey})=(\\w{0,})`, 'g').exec(window.location.href)[2];
   }
   
   // My sorting algorithms, the 'Cold Water Sorting'
@@ -244,8 +211,7 @@ function quick() {
     s_l: sortFromLowest,
     stl: sortFromLowestToHighest, // with own sorting algorithm
     sth: sortFromHighestToLowest, // the same
-    gcn: getCookieByName,
-    gpn: getParameterByName,
+    gpn: getUrlVariableValueByName,
     fj: fetchJson,
     sp: setPropertyOnStyle,
     it: setInnerText,
